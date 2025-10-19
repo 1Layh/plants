6d7d099233ffec1a2ef9675f7a4570e0f4b7c1f8
1Layh <2020ipod@gmail.com>
2025-10-19 15:19:23 +0300

    Initial Commit

---
+++ plantTests/plantTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantTests.swift
+//  plantTests
+//
+//  Created by Layan on 27/04/1447 AH.
+//
+
+import Testing
+@testable import plant
+
+struct plantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
